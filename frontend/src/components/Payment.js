6c7b5a7a3256e6371955f92b73596ce8ee894b76
srusti-joshi <srustijoshi2001@gmail.com>
2023-06-02 17:17:55 +0530

    updated payment

--- frontend/src/components/Payment.js
+++ frontend/src/components/Payment.js
@@ -1,19 +1,111 @@
-import React, { useContext } from 'react'
+import Axios from "axios";
+import React, { useContext, useState } from 'react'
 import Navbar from './Navbar'
 import './ReportDisplay.css'
-import './PaymentGateway'
+import { server } from "./Server";
+import "./PaymentGateway.css"
 import Personal from '../assets/Personal.svg'
 import Delivery from '../assets/Delivery.svg'
 import pencil from '../assets/pencil.svg'
 import { Store } from '../Store'
-import { useNavigate } from 'react-router-dom'
+//import { useNavigate } from 'react-router-dom'
 const Payment = () => {
-    const navigate=useNavigate()
+    //const navigate=useNavigate()
     const {state,dispatch:cxtDispatch}=useContext(Store)
     const {totalPrice}=state
-    const handlepay=()=>{
-      navigate('/paymentgateway')
-    }
+    const [amount, setAmount] = useState();
+    const handlePaymentSuccess = async (response) => {
+      try {
+        let bodyData = new FormData();
+  
+        // we will send the response we've got from razorpay to the backend to validate the payment
+        bodyData.append("response", JSON.stringify(response));
+  
+        await Axios({
+          url: `${server}/razorpay/payment/success/`,
+          method: "POST",
+          data: bodyData,
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        })
+          .then((res) => {
+            console.log("Everything is OK!");
+            
+            
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      } catch (error) {
+        console.log(console.error());
+      }
+    };
+  
+    // this will load a script tag which will open up Razorpay payment card to make //transactions
+    const loadScript = () => {
+      const script = document.createElement("script");
+      script.src = "https://checkout.razorpay.com/v1/checkout.js";
+      document.body.appendChild(script);
+    };
+    const showRazorpay = async () => {
+      const res = await loadScript();
+  
+      let bodyData = new FormData();
+      console.log(amount);
+      setAmount({totalPrice});
+  
+  
+      // we will pass the amount and product name to the backend using form data
+      bodyData.append("amount", totalPrice.toString());
+      
+      
+      const data = await Axios({
+        url: `${server}/razorpay/pay/`,
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        data: bodyData,
+      }).then((res) => {
+        return res;
+      });
+  
+      // in data we will receive an object from the backend with the information about the payment
+      //that has been made by the user
+  
+      var options = {
+        key_id: process.env.REACT_APP_PUBLIC_KEY, // in react your environment variable must start with REACT_APP_
+        key_secret: process.env.REACT_APP_SECRET_KEY,
+        amount: data.data.payment.amount,
+        currency: "INR",
+        name: "Rajan Business Ideas Pvt. Ltd",
+        description: "Test transaction",
+        image: "", // add image url
+        order_id: data.data.payment.id,
+        handler: function (response) {
+          // we will handle success by calling handlePaymentSuccess method and
+          // will pass the response that we've got from razorpay
+          handlePaymentSuccess(response);
+        },
+        prefill: {
+          name: "User's name",
+          email: "User's email",
+          contact: "User's phone",
+        },
+        notes: {
+          address: "Razorpay Corporate Office",
+        },
+        theme: {
+          color: "#3399cc",
+        },
+      };
+  
+      var rzp1 = new window.Razorpay(options);
+      rzp1.open();
+    };
   return (
     <div>
     <Navbar reports/>
@@ -51,7 +143,7 @@ const Payment = () => {
       <p className='pay-price'>Total Price:₹{totalPrice}</p>
       </div>
       <div className='row'>
-       <button onClick={handlepay} className='pay-btn '>PAY NOW</button>
+       <button onClick={showRazorpay} className='pay-btn '>PAY NOW</button>
       </div>
       </div>
      </div>
