b3db6fe1383107c5d4457bcf986a44e3fc8b0482
srusti-joshi <srustijoshi2001@gmail.com>
2023-06-17 15:36:25 +0530

    static

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS=[
-os.path.join(BASE_DIR,'frontend/build/static'),
+os.path.join(BASE_DIR,'static'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
