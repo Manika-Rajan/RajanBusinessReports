9b6f898688d3d88211fe864dd72033846e664bf2
srusti-joshi <srustijoshi2001@gmail.com>
2023-06-17 15:58:33 +0530

    deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS=[
-os.path.join(BASE_DIR,'static'),
+'/home/ubuntu/RajanBusinessReports/staticfiles',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
