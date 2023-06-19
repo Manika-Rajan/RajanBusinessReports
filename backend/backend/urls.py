8b25aed9996c5520b2acce5aecfa566cca73f6b6
srusti-joshi <srustijoshi2001@gmail.com>
2023-06-19 12:20:12 +0530

    deploy

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,17 +18,17 @@ from django.contrib import admin
 from django.urls import path,include
 from django.views.generic import RedirectView
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
-from django.conf.urls import url
-from django.views.static import serve
+#from django.conf.urls.static import static
+#from django.conf import settings
+#from django.conf.urls import url
+#from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('razorpay/', include("api.urls")),
     path('', views.index,name='index'),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    #url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     
 ]
-urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+#urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
